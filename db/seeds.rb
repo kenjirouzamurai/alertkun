61ce9332c35008de384d51b4b839abdad9dbf04c
山田健二 <kyamada@rivelty.jp>
2024-11-09 15:11:18 +0900

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 company = Company.create!(name: "リベルティ", plan: :pro)
-company_1 = Company.create!(name: "株式会社ポップコーン", plan: :pro)
 
 user = User.create!(
   email: "kyamada@rivelty.jp",
   password: "hogehoge",
   company: company,
-  line_id: "Uc27650c7228e0cbf772ee45a4c3054b1"
+  line_id: "Ube7f939273b87bf16333aa3d0767ae8d"
 )
 
-# user.sites.create(name: "ホームページ", domain: "https://rivelty.jp")
-
-osawa = User.create!(
-  email: "yohei.osawa@gmail.com",
-  password: "password",
-  company: company_1
-)
-# osawa.sites.create(name: "life", domain: "https://womanlife.online")
+user.sites.create(name: "ホームページ", domain: "https://rivelty.jp")
