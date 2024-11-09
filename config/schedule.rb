6dfa7a53c1a2d1c61b86bfd5c04df88bcf8c8d5a
山田健二 <kyamada@rivelty.jp>
2024-11-09 17:41:40 +0900

    fix

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,6 +6,6 @@ require File.expand_path(File.dirname(__FILE__) + '/environment')
 set :environment, :production
 set :output, "#{Rails.root}/log/cron.log"
 
-every 10.minutes do
+every 20.minutes do
   rake 'ping:health_check'
 end
